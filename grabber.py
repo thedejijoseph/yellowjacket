7ab3263bdab27c8bbae98f2db4a1669eb7565e12
Deji <wrecodde@gmail.com>
2020-05-20 03:42:44 +0100

    added logging

--- grabber.py
+++ grabber.py
@@ -1,6 +1,20 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import os
+import logging
 from datetime import datetime
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+stream = logging.StreamHandler()
+stream_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream.setFormatter(stream_formatter)
+
+logger.addHandler(stream)
+
 
 def page_loaded(driver):
     try:
@@ -62,7 +76,7 @@ def get_decliners(soup=None):
 """
 todo:
     + code retry-on-failure feature into grabber block
-    introduce logging script-wide
+    + introduce logging script-wide
     introduce env variables into webdriver creation
     introduce database saving into project
 """
@@ -73,46 +87,56 @@ def run():
     options.add_argument('--no-sandbox')
     options.add_argument('--remote-debugging-port=9222')
     options.add_argument('--log-level=3')
+
+    logger.debug('Opening web driver.')
     driver = webdriver.Chrome('drivers/chromedriver81.exe', options=options)
     driver.get('http://www.nse.com.ng')
 
-    number_of_retries = 12
+    number_of_retries = 36
     try_count = 0
     while try_count <= number_of_retries:
         if page_loaded(driver):
             break
         else:
+            logger.warning(f'Could not reach the internet. Retrying. Count: {try_count}.')
             if try_count == number_of_retries:
                 # log iinability to reach internet and terminate
+                logger.critical('Could not reach the internet. Terminating.')
                 return
             
             driver.get('http://www.nse.com.ng')
             try_count += 1
             continue
-    
+    logger.info('Fetched web page. Parsing.')
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     log_time = str(datetime.now())
 
+    logger.debug('Saving Advancers data.')
     advancers = get_advancers(soup=soup)
     with open('data/advancers.csv', 'a') as f:
         for row in advancers:
             f.write(f'{log_time}; {row[0]}; {row[1]}; {row[2]}; {row[3]}; {row[4]}\n')
     
+    logger.debug('Saving Decliners data.')
     decliners = get_decliners(soup=soup)
     with open('data/decliners.csv', 'a') as f:
         for row in decliners:
             f.write(f'{log_time}; {row[0]}; {row[1]}; {row[2]}; {row[3]}; {row[4]}\n')
 
+    logger.debug('Saving Index Snapshot data.')
     snapshot = get_snapshot(soup=soup)
     with open('data/snapshots.csv', 'a') as f:
         for row in snapshot:
             f.write(f'{log_time}; {row[0]}; {row[1]}\n')
     
+    logger.debug('Saving Trades data.')
     trades = get_trades(soup=soup)
     with open('data/trades.csv', 'a') as f:
         for row in trades:
             f.write(f'{log_time}; {row[0]}; {row[1]}; {row[2]}\n')
     
+    logger.info('Finished saving data.')
+    logger.debug('Closing web driver.')
     driver.quit()
 
 if  __name__ == "__main__":
